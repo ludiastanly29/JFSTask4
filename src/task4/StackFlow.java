aa11cf086cf1406070b88ce2f06589d5829a5e1f
Ludia <160195657+ludiastanly29@users.noreply.github.com>
2024-02-24 01:07:52 +0530

    Update StackFlow.java

--- src/task4/StackFlow.java
+++ src/task4/StackFlow.java
@@ -8,45 +8,69 @@ public class StackFlow {
 	public static void main(String[] args) {
 		// Create a stack to store integers
 		Stack<Integer> integerStack = new Stack<>();
-        Scanner scanner = new Scanner(System.in);
+        	Scanner scanner = new Scanner(System.in);
 	   	while (true) {
-	   		System.out.println("\nStack flow Menu:");
-            System.out.println("1. Add a number ");
-            System.out.println("2. Remove a number ");
-            System.out.println("3. is Empty");
-            System.out.println("4. Exit");
-            System.out.print("Enter your choice: ");
-
-            int choice = scanner.nextInt();
-            scanner.nextLine();  // Consume newline character
-            switch (choice) {
-	            case 1:
-	            	// Push elements onto the stack
-	            	System.out.print("Enter the number: ");
-	                int number = scanner.nextInt();
-	                pushElements(integerStack,number);
-	                System.out.println("Displaying the numbers in the stack: "+integerStack);
-	                break;
-	            case 2:
-	            	// Pop elements from the stack
-	                popElements(integerStack);
-	                System.out.println("Displaying the numbers in the stack: "+integerStack);
-	                break;
+	   	    System.out.println("\nStack flow Menu:");
+	            System.out.println("1. Add a number ");
+	            System.out.println("2. Remove a number ");
+	            System.out.println("3. is Empty");
+	            System.out.println("4. Exit");
+	            System.out.print("Enter your choice: ");
 	
-	            case 3:
-	            	// Check if the stack is empty
-	            	boolean isEmpty = integerStack.isEmpty();
-	            	System.out.println("Is the stack empty? " + isEmpty);
-	            	System.out.println("Displaying the numbers in the stack: "+integerStack);
-	                break;
-	
-	            case 4:
-	                System.out.println("Exiting the program. Goodbye!");
-	                System.exit(0);
-	
-	            default:
-	                System.out.println("Invalid choice. Please enter a valid option.");
-	        }
+	            int choice = scanner.nextInt();
+	            scanner.nextLine();  // Consume newline character
+	            switch (choice) {
+		            case 1:
+		            	// Push elements onto the stack
+		            	System.out.print("Enter the number: ");
+		                int number = scanner.nextInt();
+		                pushElements(integerStack,number);
+		                System.out.println();
+		  	        System.out.println("------------------------------------------");
+		  	        System.out.println();
+		                System.out.println("Displaying the numbers in the stack: "+integerStack);
+		                System.out.println();
+		  	        System.out.println("------------------------------------------");
+		  	        System.out.println();
+		                break;
+		            case 2:
+		            	// Pop elements from the stack
+		                popElements(integerStack);
+		                System.out.println();
+		  	        System.out.println("------------------------------------------");
+		  	        System.out.println();
+		                System.out.println("Displaying the numbers in the stack: "+integerStack);
+		                System.out.println();
+		  	        System.out.println("------------------------------------------");
+		  	        System.out.println();
+		                break;
+		
+		            case 3:
+		            	// Check if the stack is empty
+		            	boolean isEmpty = integerStack.isEmpty();
+		            	System.out.println("Is the stack empty? " + isEmpty);
+		            	System.out.println();
+		  	        System.out.println("------------------------------------------");
+		  	        System.out.println();
+		            	System.out.println("Displaying the numbers in the stack: "+integerStack);
+		            	System.out.println();
+		  	        System.out.println("------------------------------------------");
+		  	        System.out.println();
+		                break;
+		
+		            case 4:
+		                System.out.println("Exiting the program. Goodbye!");
+		                System.out.println();
+		  	        System.out.println("------------------------------------------");
+		  	        System.out.println();
+		                System.exit(0);
+		
+		            default:
+		                System.out.println("Invalid choice. Please enter a valid option.");
+		                System.out.println();
+		  	        System.out.println("------------------------------------------");
+		  	        System.out.println();
+		        }
 	   	 }
 	}
 
